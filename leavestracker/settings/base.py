0a194b76c05f0914e8000074f397b28ae5d719a4
HetPaghadal <het.paghadal@squadstack.com>
2022-01-26 14:14:40 +0530

    Deploy

--- leavestracker/settings/base.py
+++ leavestracker/settings/base.py
@@ -89,6 +89,7 @@ TEMPLATES = [
 WSGI_APPLICATION = "leavestracker.wsgi.application"
 
 CORS_ALLOW_CREDENTIALS = True
+CORS_ORIGIN_ALLOW_ALL = True
 
 CORS_ORIGIN_WHITELIST = [
     'https://localhost:3000',
