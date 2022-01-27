d39c85a050dd02183af9ea025fb52ccc3181edb0
HetPaghadal <het.paghadal@squadstack.com>
2022-01-27 18:15:48 +0530

    Deploy

--- leavestracker/settings/base.py
+++ leavestracker/settings/base.py
@@ -15,7 +15,7 @@ import os
 import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
@@ -164,7 +164,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = (
+    os.path.join(BASE_DIR, 'static'),
+)
 STATIC_URL = "static/"
 
 # Default primary key field type
