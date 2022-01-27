4c42a6e01349a5b0a109ab3d21368a25d7f2306b
HetPaghadal <het.paghadal@squadstack.com>
2022-01-27 19:01:14 +0530

    Local

--- leavestracker/settings/local.py
+++ leavestracker/settings/local.py
@@ -3,18 +3,18 @@ from .local_example import *
 DATABASES = {
     "default": {
         "ENGINE": DB_ENGINE,
-        "NAME": "d4t1e3iggg5hg9",
-        "USER": "qdbiodwiqfupzo",
-        "PASSWORD": "26b5c8ca517c8516e3f6de1d03310a963289c07789eecf3cd068c44c66fc91cf",
-        "HOST": "ec2-54-208-139-247.compute-1.amazonaws.com",
+        "NAME": DB_NAME,
+        "USER": "myuser",
+        "PASSWORD": "mypass",
+        "HOST": "localhost",
         "PORT": "5432",
     },
     "readonly": {
         "ENGINE": DB_ENGINE,
-        "NAME": "d4t1e3iggg5hg9",
-        "USER": "qdbiodwiqfupzo",
-        "PASSWORD": "26b5c8ca517c8516e3f6de1d03310a963289c07789eecf3cd068c44c66fc91cf",
-        "HOST": "ec2-54-208-139-247.compute-1.amazonaws.com",
+        "NAME": DB_NAME,
+        "USER": "myuser",
+        "PASSWORD": "mypass",
+        "HOST": "localhost",
         "PORT": "5432",
     },
 }
